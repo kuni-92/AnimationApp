4394d857e7e2e8241ab390249176aea5588d6908
kunihiro-dev <49485046+kunihiro-dev@users.noreply.github.com>
2022-06-06 21:04:56 +0900

    add rectangle animation

--- AnimationApp/ContentView.swift
+++ AnimationApp/ContentView.swift
@@ -16,8 +16,17 @@ struct ContentView: View {
                 .fill(Color.blue)
                 .frame(width: 200, height: 200)
                 .scaleEffect(scaleFlag ? 1.0 : 2.0)
+            Circle()
+                .fill(Color.red)
+                .frame(width: 200, height: 200)
+                .scaleEffect(scaleFlag ? 1.0 : 0.5)
+            Rectangle()
+                .fill(Color.green)
+                .frame(width: 100, height: 100)
+                .rotationEffect(Angle(degrees: scaleFlag ? 0 :180))
+                .animation(.spring(), value: self.scaleFlag)
             Spacer()
-            Button("Animation Start") {
+            Button("Flag ON") {
                 withAnimation {
                     self.scaleFlag.toggle()
                 }
